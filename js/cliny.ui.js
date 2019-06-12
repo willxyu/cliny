a32f2a874c89d240f1c8cc086971b7d07f8fa8fe
willxyu <willxyu@users.noreply.github.com>
2019-06-12 18:16:12 +1000

    Major update to filterise()
    Not keen on adding logic to ui functions but whatever.

--- js/cliny.ui.js
+++ js/cliny.ui.js
@@ -8,6 +8,7 @@
   - showSource()
   - hideSource()
  */
+_state_of_all_ = typeof _state_of_all != 'undefined' ? _state_of_all_ : true  // Lol
 
 reply = function() {
   var answers = MCQ.A
@@ -26,8 +27,9 @@ reverse = function() {
 }
 
 filterise = function() {
-  var template = '<li class="li-selector"><label class="switch"><input class="in-check" type="checkbox" checked="checked" data="DATUM"><span class="slider round"></span></label><div class="selector">CONTENT</div></li>'
+  var template = '<li class="li-selector"><label class="switch"><input class="in-check DATUM" type="checkbox" checked="checked" data="DATUM"><span class="slider round"></span></label><div class="selector">CONTENT</div></li>'
   var s = ''
+  var r = /DATUM/g // not super concerned about being hyper-performant so generating regex here is fine
   for (var k in perms) {
     var t = k
     if (typeof cats != 'undefined' && 
@@ -35,14 +37,46 @@ filterise = function() {
         typeof cats[k].length == 'number') {
       t = k + ' (' + cats[k].length + ')'
     }
-    s += template.replace('CONTENT', t).replace('DATUM', k) 
+    s += template.replace('CONTENT', t).replace(r, k)
   }
-  s = template.replace('CONTENT', 'All').replace('DATUM', 'All') + s
+  s = template.replace('CONTENT', 'All').replace(r, 'All') + s
   $('#q-filter').empty().append(s)
+  for (var k in perms) {
+    $('.li-selector .in-check.' + k).prop('checked', perms[k])
+  }
+  $('.li-selector .in-check.All').prop('checked', _state_of_all_)
+    
+  // Remodel behaviours
+  $('#q-filter li input').off('change').change(function() {
+    if (this.checked == 'checked') { this.checked = true }
+  
+    var header = this.getAttribute('data')
+    if (header == 'All') {
+    } else {
+    if (this.checked) {
+      perms[header] = true
+    } else {
+      perms[header] = false
+    } }
+    storageSave(perms, 'cliny.perms')
+  })
+  $('.in-check.All').off('change').change(function() {
+    if (this.checked) {
+      _state_of_all_ = true
+      $('#q-filter .in-check').prop('checked', true)
+      for (var k in perms) { perms[k] = true }
+    } else {
+      _state_of_all_ = false
+      $('#q-filter .in-check').prop('checked', false)
+      for (var k in perms) { perms[k] = false }
+    }
+    storageSave(perms, 'cliny.perms')
+  })
 }
 
 showFilter = function() {
   $('#q-util').empty().append('&rHar;')
+  filterise()
   $('#q-filter').css('display','block')
 }
 
